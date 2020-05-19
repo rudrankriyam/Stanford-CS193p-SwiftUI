90b19b84c089bb7f2498f9d7036df7acc6ae7c32
Rudrank <rudrankriyam@gmail.com>
2020-05-19 10:30:07 +0530

    Initial commit of Memorize

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Rudrank Riyam on 19/05/20.
+//  Copyright © 2020 Rudrank Riyam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
