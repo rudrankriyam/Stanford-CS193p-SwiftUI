b804306f0fd0dbf2d952e67ea8c8597f79fb37a5
Rudrank <rudrankriyam@gmail.com>
2020-05-19 11:29:39 +0530

    Lecture 1 Course Logistics and Intro to SwiftUI

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,12 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: index % 2 == 0)
+            }
+        }
+        .font(.largeTitle)
+        .foregroundColor(.orange)
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(Color.white)
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(lineWidth: 3)
+            Text("👻")
+            } else {
+            RoundedRectangle(cornerRadius: 16)
+                          .fill()
+            }
+        }
     }
 }
